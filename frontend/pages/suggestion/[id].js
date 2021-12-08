0a2a7f6ca6a16348285b0c85479930c63cbb1315
jljakin2 <jeff.jakinovich@gmail.com>
2021-12-07 21:30:38 -0500

    comments page started and temp data is connected

--- frontend/pages/suggestion/[id].js
+++ frontend/pages/suggestion/[id].js
@@ -1,8 +1,49 @@
+import Head from "next/head";
+import styled from "styled-components";
 import { useRouter } from "next/router";
+
+import GoBackBtn from "../../components/Buttons/GoBackBtn";
+import EditFeedbackBtn from "../../components/Buttons/EditFeedbackBtn";
+import Comments from "../../components/Comments";
+
+import data from "../../lib/data.json";
+import SuggestionCard from "../../components/SuggestionCard";
+
+const PageStyles = styled.div`
+  padding: 1.5rem 1.5rem;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
 
 export default function SuggestionPage() {
   const router = useRouter();
-  const { id } = router.query;
+  let { id } = router.query;
 
-  return <p>specific issue: {id}</p>;
+  const products = data.productRequests;
+  const product = products.filter(item => {
+    return item.id == id;
+  })[0];
+
+  return (
+    <PageStyles>
+      <Head>
+        <title>Product Feedback | Suggestion: {id}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <GoBackBtn />
+        <EditFeedbackBtn />
+      </header>
+
+      <main>
+        {product && <SuggestionCard product={product} />}
+        {product && <Comments comments={product.comments} />}
+      </main>
+    </PageStyles>
+  );
 }
