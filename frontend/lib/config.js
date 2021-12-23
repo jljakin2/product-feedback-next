1e5e95910400473c30d5dde6ffb36e52c3a9bd78
jljakin2 <jeff.jakinovich@gmail.com>
2021-12-23 14:17:17 -0500

    added id to current user object in config file

--- frontend/lib/config.js
+++ frontend/lib/config.js
@@ -14,6 +14,7 @@ export const filterOptions = [
 ];
 
 export const user = {
+  id: "61ba6848fe09b43f32591efe",
   image:
     "res.cloudinary.com/dpp64ouz9/image/upload/v1639702185/productFeedback/image-zena_t0xqs8.jpg",
   name: "Zena Kelley",
