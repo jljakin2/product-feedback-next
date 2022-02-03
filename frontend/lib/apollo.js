e9dc56d2e0731dc9fc8618fcf2958957f2693a44
jljakin2 <jeff.jakinovich@gmail.com>
2022-02-02 19:56:51 -0500

    cleanup for vercel deployment

--- frontend/lib/apollo.js
+++ frontend/lib/apollo.js
@@ -1,12 +1,3 @@
-// import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-// const apolloClient = new ApolloClient({
-//   uri: "http://localhost:3000/admin/api",
-//   cache: new InMemoryCache(),
-// });
-
-// export default apolloClient;
-
 import { useMemo } from "react";
 import createApolloClient from "./apolloClient";
 
